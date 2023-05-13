2e0768bb7c58cf64791a2479b2aea4013ed6c187
Ved <vedbulsara7@gmail.com>
2023-05-13 08:19:21 +0530

    commit 1.0 (13/5/23)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+  const [color, setColor] = useState("red");
+
+  return(
+    <>
+      <h1>My favourite color is {color}!</h1>
+      
+      <button
+        type='button'
+        onClick={() => setColor("blue")}
+      >blue
+      </button>
+
+      <button
+        type='button'      
+        onClick={() => setColor("red")}
+      >red      
+      </button>
+
+      <button
+        type='button'
+        onClick={() => setColor("yellow")}
+      >yellow        
+      </button>
+    </>
   );
 }
 
